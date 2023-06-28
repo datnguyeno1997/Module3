c01f349faff6b5c294d9f3b91cb3dfe896e35a64
datnguyeno1997 <tn6248293@gmail.com>
2023-06-28 16:48:42 +0700

    up

---
+++ Truy vấn dữ liệu với Quản lý Sinh Viên.sql
@@ -0,0 +1,26 @@
+USE QuanLySinhVien;
+
+SELECT *
+FROM Student;
+
+SELECT *
+FROM student
+WHERE Status = true;
+
+SELECT * 
+FROM Subject
+WHERE Credit < 10;
+
+SELECT S.StudentId, S.StudentName, C.ClassName
+FROM Student S join Class C on S.ClassId = C.ClassID;
+
+SELECT S.StudentId, S.StudentName, C.ClassName
+FROM Student S join Class C on S.ClassID = C.ClassID
+WHERE C.ClassName = 'A1';
+
+SELECT S.StudentId, S.StudentName, Sub.SubName, M.Mark
+FROM Student S join Mark M on S.StudentId = M.Student join Subject Sub on M.SubId = Sub.SubId;
+
+SELECT S.StudentId, S.StudentName, Sub.SubName, M.Mark
+FROM Student S join Mark M on S.StudentId = M.StudentId join Subject Sub on M.SubId = Sub.SubId
+WHERE Sub.SubName = 'CF';
